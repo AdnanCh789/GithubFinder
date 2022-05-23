513c3f980fa525e01ace9a6330556e11e826ed42
Adnan Aslam <zasadnanch789@gmail.com>
2022-05-23 16:24:36 +0500

    used ui.js & git API to make app

---
+++ app.js
@@ -0,0 +1,32 @@
+const github = new Github;
+const ui = new UI;
+
+const searchUser = document.getElementById('searchUser');
+searchUser.addEventListener('keyup', (e) => {
+    const userText = e.target.value;
+    
+    if(userText !== '') {
+        github.getUser(userText)
+        .then( data => {
+            if (data.profile.message === 'Not Found') {
+                // Show alert
+                ui.showAlert('User not found','alert alert-danger');
+            }
+            else {
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+
+            }
+            
+        })
+
+        
+
+    }
+    else {
+        // Clear the field
+        ui.clearProfile();
+    }
+   
+
+});
