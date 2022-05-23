6f2b9daf2daebbd14f0795c4f6012afe0cf86e02
Adnan Aslam <zasadnanch789@gmail.com>
2022-05-23 16:28:48 +0500

    used fetch for repositories as well

--- github.js
+++ github.js
@@ -3,15 +3,22 @@ constructor () {
 
     this.client_id = '960a3cdf8c1ecec528e9';
     this.client_secret = 'f33901be5615907a50505e027394fd5bbfa83e53';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
 }
 
 async getUser (user) {  
     const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-        profile
+        profile,
+        repos
     }
 
 }
